7a582594bd30632e7cc806cda3eb3071e55136c7
fabiogavazzi-magnasistemas <irlima@magnasistemas.com.br>
2022-05-02 16:06:02 -0300

    atualizacao 2.0

---
+++ apiMarvel.js
@@ -0,0 +1,17 @@
+
+const keyPublic = '73da05e770ee8aa50af741f985f3d075';
+const keyPrivate = '3cf7931c65386597cab0bad01e535be02738499c';
+// const hash =
+var timestamp = new Date().getTime();
+
+console.log(timestamp);
+function gerandoHash(hash){
+
+    hash = (keyPublic + keyPrivate + timestamp);
+    return gerandoHash
+}
+
+
+
+                  
+         
