ac03207ed24a9cc4d06c47e5bf85d458454666f8
fabiogavazzi-magnasistemas <irlima@magnasistemas.com.br>
2022-05-03 16:00:06 -0300

    atualizacao

--- apiMarvel.js
+++ apiMarvel.js
@@ -1,17 +1,24 @@
-
-const keyPublic = '73da05e770ee8aa50af741f985f3d075';
-const keyPrivate = '3cf7931c65386597cab0bad01e535be02738499c';
-// const hash =
-var timestamp = new Date().getTime();
-
-console.log(timestamp);
-function gerandoHash(hash){
-
-    hash = (keyPublic + keyPrivate + timestamp);
-    return gerandoHash
+function getParams() {
+  const keyPublic = "73da05e770ee8aa50af741f985f3d075";
+  const keyPrivate = "3cf7931c65386597cab0bad01e535be02738499c";
+  const ts = new Date().getTime();
+  const hash = md5(`${ts}${keyPrivate}${keyPublic}`);
+  return {
+    ts,
+    keyPublic,
+    hash,
+  };
 }
 
+function getApi(params) {
+  const res = new XMLHttpRequest();
+  res.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const response = JSON.parse(res.responseText);
+    
+    }
+  };
+  res.open("GET",`http://gateway.marvel.com/v1/public/comics?ts=${params.ts}&apikey=${params.keyPublic}&hash${params.hash}`, true);
+  res.send();
+}
 
-
-                  
-         
