40dd1301cf32d8986a2f9a044ef05c5efe619c14
fabiogavazzi-magnasistemas <irlima@magnasistemas.com.br>
2022-05-09 16:55:02 -0300

    commit de atualização

--- apiMarvel.js
+++ apiMarvel.js
@@ -1,32 +1,54 @@
 function getParams() {
-  const keyPublic = "73da05e770ee8aa50af741f985f3d075";
-  const keyPrivate = "3cf7931c65386597cab0bad01e535be02738499c";
+  const apiKey = '73da05e770ee8aa50af741f985f3d075';
+  const privateKey = '3cf7931c65386597cab0bad01e535be02738499c';
   const ts = new Date().getTime();
-  const hash = md5(`${ts}${keyPrivate}${keyPublic}`);
+  const hash = md5(`${ts}${privateKey}${apiKey}`);
+  
   return {
     ts,
-    keyPublic,
+    apiKey,
     hash,
   };
+
 }
-
-
-function getApi() {
+function getApi(params) {
   const res = new XMLHttpRequest();
-  const url= `http://gateway.marvel.com/v1/public/characters?ts=${params.ts}&apikey=${params.keyPublic}&hash=${params.hash}`;
+  const url = `http://gateway.marvel.com/v1/public/characters?ts=${params.ts}&apikey=${params.apiKey}&hash=${params.hash}`;
   res.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      const response = JSON.parse(res.responseText); 
-      getCard(response)
+      const response = JSON.parse(res.responseText);
+     const heroi = document.querySelector("#heroi");
+      response.data.results.forEach(params => {
+        const srcImg = params.thumbnail.path + '.'+ params.thumbnail.extension;
+        const heroiName= params.name;
+        getCard(srcImg , heroiName, heroiName);
+      });    
+      console.log(response.data.results)
     }
-  };
-  res.open("GET",url,true); 
+
+    };
+  res.open("GET", url, true);
   res.send();
 }
+
 const params = getParams();
-const requisicao = getApi(params);
+getApi(params);
 
 
-function getCard(response){
+function getCard(srcImg ,heroiName, heroiDiv){
+  const colPai = document.createElement('div');
+  const colFilho = document.createElement('div');
+  const text = document.createElement('h3');
+  const textChild = document.createElement('p');
+  const img = document.createElement('img');
+  text.textContent = heroiName
+  img.src=srcImg
+  colPai.append(colFilho)
+  colFilho.append(text)
+  colFilho.append(textChild)
+  colFilho.append(img)
+  heroiDiv.append(colFilho)
+  colPai.classList.add(personagem);
 
 }
+getCard()
