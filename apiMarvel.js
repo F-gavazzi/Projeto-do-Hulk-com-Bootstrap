f9e077bde8c8de18eaa227f6818370bba37597c3
fgavazzi <fgavazzi@magnasistemas.com.br>
2022-05-11 10:02:47 -0300

    Inserindo tabela e imagem

--- apiMarvel.js
+++ apiMarvel.js
@@ -10,146 +10,101 @@ function getParams() {
     hash,
   };
 }
-function cretePersonagem() {
-  /////////////////////////////////////////////CONTENT//////////////////////////////////////////////  
+function createPersonagem(personagem){
+  /////////\\\\\////////////////ID\\\\\\/\/\/\//\/\/\/\/\////ID\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\ID/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
   const content = document.querySelector("#content");
   const list = document.querySelector("#list");
-
-  const title = document.createElement("h4");
-  title.classList.add("mt-2", "h3");
+//\/\//\/\/\//\/\/\/\//\/\/\/\//\/\/\/CONTENT/\/\/\/\/\/\/\/\/\/\/\/\/\/\/CONTENT\/\/\/\//\//\/\/\//\/\/\/\/\//\/\/\//\/\/\//\\/\
+  const title = document.createElement("h3");
+  title.classList.add("mt-2", "h5");
   title.innerText = "Personagem Marvel";
+  
+  const namePersonagem =document.createElement("h1")
+  namePersonagem.classList.add("mt-2");
+  namePersonagem.innerText= personagem.name;
+  
+  const text =document.createElement("p")
+  text.classList.add("h5", "mt-3")
+  text.innerText= personagem.description
 
-  const name = document.createElement("h1");
-  name.classList.add("h1");
-  name.innerText = "Hulk";
-
-  const img = document.createElement("img");
-  img.src = "./img/personagem.jpg";
+  const img =document.createElement("img")
   img.classList.add("img-fluid");
+  img.src = `${personagem.thumbnail.path}.${personagem.thumbnail.extension}`;
+////////\/\/\/\/\/\//\/\/\/\/\/\/\/TABLE\//\/\/\/;\/\/\/\/\/\/\/\/\/\//\/\/\\/\/\///\/\\//\/\/\/\//\/\//\//\
+  const historias = document.createElement("h2");
+  historias.classList.add("mt-4", "mb-2", "text-center");
+  historias.innerText = "Histórias";
 
-  const text = document.createElement("p");
-/////////////////////////////////////////////TABLE//////////////////////////////////////////////////
-  const table = document.createElement("table");
-  table.classList.add("table", "table-bordered", "mt-4", "text-center");
-
-  const thead = document.createElement("thead");
-  const tr = document.createElement("tr");
-  const contentTable = ["ID", "Titulo", "Tipo"];
-
-  contentTable.forEach((item) => {
-    const th = document.createElement("th");
-    th.innerText = item;
-
-    tr.append(th);
-  });
-  const tbody = document.createElement("tbody");
-  const contentTbody = [
-    { id: "398123", titulo: "titulo comic", tipo: "tipo comic" },
-    { id: "398123", titulo: "titulo comic", tipo: "tipo comic" },
-  ];
-  contentTbody.forEach((item) => {
+    const table = document.createElement("table");
+    table.classList.add("table", "table-bordered", "mt-4");
+  
+    const thead = document.createElement("thead");
     const tr = document.createElement("tr");
+  
+    const tableTitle = ["ID", "Titulo", "Tipo"];
+  
+    tableTitle.forEach(element => {
+      const th = document.createElement("th");
+      th.innerText = element;
+  
+      tr.append(th); 
+    }); 
+  
+    const tbody = document.createElement("tbody");
+  
+  
+    personagem.stories.items.forEach(element => {
+      const tr1 = document.createElement("tr");
+  
+      const td1 = document.createElement("td");
+      td1.innerText = urlId(element.resourceURI);
+  
+      const td2 = document.createElement("td");
+      td2.innerText = element.name;
+  
+      const td3 = document.createElement("td");
+      td3.innerText = element.type;
+  
+      tr1.append(td1);
+      tr1.append(td2);
+      tr1.append(td3);
+  
+      tbody.append(tr1)
+      
+    })
+  
 
-    const td1 = document.createElement("td");
-    td1.innerText = item.id;
+  content.append(title)
+  content.append(namePersonagem)
+  content.append(text)
+  content.append(img)
+  content.append(historias)
+  content.append(tbody)
 
-    const td2 = document.createElement("td");
-    td2.innerText = item.titulo;
-
-    const td3 = document.createElement("td");
-    td3.innerText = item.tipo;
-
-    tr.append(td1);
-    tr.append(td2);
-    tr.append(td3);
-    tbody.append(tr);
-  });
-
-  const divTable = document.createElement("div");
-  divTable.classList.add("text-center", "align-center");
-
-
-
-/////////////////////////////////////////////////////LISTA/////////////////////////////////////////
-  const h3 = document.createElement("h3");
-  // h3.classList.add("-center")
-  h3.innerText = "Histórias";
-
-  const ul = document.createElement("ul");
-  ul.classList.add("ml-2");
-
-  const lista = [
-    "FREE COMIC BOOK DAY 2013 1 (2013) #1",
-    "Hulk (2008) #53",
-    "Hulk (2008) #54",
-    "Hulk (2008) #55",
-  ];
-  lista.forEach((item) => {
-    const li1 = document.createElement("li");
-    li1.innerText = item;
-
-    ul.append(li1);
-  });
-
-  const h4List=document.createElement("h4");
-  h4List.classList.add("pb-2" , "text-white" , "mt-4" );
-  h4List.innerText = "Lista de Aparições (comics)"
-
-  const divList = document.createElement("div");
-  divList.classList.add("text-white", "mt-2" , "ml-2");
-
-
-  ///////////////////////////////////////////////////APPEND/////////////////////////////////////////////
-  content.append(title);
-  content.append(name);
-  content.append(text);
-  content.append(img);
-  divTable.append(h3);
-  divTable.append(table);
-  content.append(divTable);
-  thead.append(tr);
-  table.append(thead);
-  table.append(tbody);
-
-  list.append(h4List)
-  divList.append(ul);
-  list.append(divList);
 }
-cretePersonagem();
-
 
 function urlSepara(url){
   const id = url.split("/")
   const item = id.length -1 
 
+  return id[item];
 }
-
 
 function getApi(params) {
   const res = new XMLHttpRequest();
-  const url = `http://gateway.marvel.com/v1/public/characters/?ts=${params.ts}&apikey=${params.apiKey}&hash=${params.hash}`;
-  res.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      const response = JSON.parse(res.responseText);
-     const heroi = document.querySelector("#heroi");
-      response.data.results.forEach(params => {
-        const srcImg = params.thumbnail.path + '.'+ params.thumbnail.extension;
-        const heroiName= params.name;
-        getCard(srcImg , heroiName, heroiName);
-      });    
-      console.log(response.data.results)
-    }
+  const url = `http://gateway.marvel.com/v1/public/characters/1009368?ts=${params.ts}&apikey=${params.apiKey}&hash=${params.hash}&limit=100`;
 
-    };
-  res.open("GET", url, true);
+  res.onreadystatechange = function () {
+    if (res.readyState == 4 && res.status == 200) {
+      const response = JSON.parse(res.responseText);
+      console.log(response.data.results[0]);
+      createPersonagem(response.data.results[0]);
+    }
+  };
+  res.open(
+    "GET" , url , true );
   res.send();
 }
 
-const params = getParams();
-getApi(params);
-
-
-
-// whphpherpgher
-
-
+  const apiCalls = getParams();
+  getApi(apiCalls);
